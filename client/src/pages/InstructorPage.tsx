73a6bc19f284123e9f44c5c93d8e228c7a93ab30
laristizabal02 <lauris.paz@hotmail.es>
2025-02-25 20:22:53 -0700

    button to logout on the instructor page

--- client/src/pages/InstructorPage.tsx
+++ client/src/pages/InstructorPage.tsx
@@ -9,9 +9,15 @@ const InstructorPage = () => {
 
   return (
     <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-lg-10">
+      <div className="flex-column justify-flex-start min-100-vh">
         <div className="card p-4">
           <h2>You are on the Instructor page</h2>
+          <button
+            className="btn btn-danger mt-3"
+            onClick={() => Auth.logout()} // Call logout function
+          >
+            Logout
+          </button>
         </div>
       </div>
     </main>
