b39d4e8f0a2afa07d0cf871e87c3894cb25d65a1
laristizabal02 <lauris.paz@hotmail.es>
2025-02-25 17:40:35 -0700

    adding server login

---
+++ server/src/schemas/resolvers.ts
@@ -0,0 +1,62 @@
+import { signToken, AuthenticationError } from '../utils/auth.js'; 
+import { User } from '../models/index.js';
+
+interface AddUserArgs {
+    input:{
+      username: string;
+      email: string;
+      password: string;
+    }
+  }
+
+interface LoginUserArgs {
+    email: string;
+    password: string;
+  }
+
+
+
+  const resolvers = {
+ 
+
+    Mutation: {
+        addUser: async (_parent: any, { input }: AddUserArgs) => {
+          // Create a new user with the provided username, email, and password
+          const user = await User.create({ ...input });
+        
+          // Sign a token with the user's information
+          const token = signToken(user.username, user.email, user._id);
+        
+          // Return the token and the user
+          return { token, user };
+        },
+        
+        login: async (_parent: any, { email, password }: LoginUserArgs) => {
+          // Find a user with the provided email
+          const user = await User.findOne({ email });
+        
+          // If no user is found, throw an AuthenticationError
+          if (!user) {
+            throw new AuthenticationError('Could not authenticate user.');
+          }
+        
+          // Check if the provided password is correct
+          const correctPw = await user.isCorrectPassword(password);
+        
+          // If the password is incorrect, throw an AuthenticationError
+          if (!correctPw) {
+            throw new AuthenticationError('Could not authenticate user.');
+          }
+        
+          // Sign a token with the user's information
+          const token = signToken(user.username, user.email, user._id);
+        
+          // Return the token and the user
+          return { token, user };
+        },
+
+    },
+
+  };
+
+  export default resolvers;
