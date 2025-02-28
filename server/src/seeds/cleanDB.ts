03824bb71ba382209f9a9ce85c96222df7b16673
Steven Gray <stevegray@granite-mtn.com>
2025-02-27 20:39:13 -0700

    Added instructors to cleanDB.ts and seed.ts

--- server/src/seeds/cleanDB.ts
+++ server/src/seeds/cleanDB.ts
@@ -1,4 +1,4 @@
-import { Person, User } from '../models/index.js';
+import { Course, Instructor, Parent, Person, Student, User } from '../models/index.js';
 import process from 'process';
 
 const cleanDB = async (): Promise<void> => {
@@ -8,9 +8,21 @@ const cleanDB = async (): Promise<void> => {
     // Delete documents from User collection
     await User.deleteMany({});
     console.log('User collection cleaned.');
+    // Delete documents from Student collection
+    await Student.deleteMany({});
+    console.log('Student collection cleaned.');
+    // Delete documents from Parent collection
+    await Parent.deleteMany({});
+    console.log('Parent collection cleaned.');
+    // Delete documents from Instructor collection
+    await Instructor.deleteMany({});
+    console.log('Instructor collection cleaned.');
     // Delete documents from Person collection
     await Person.deleteMany({});
     console.log('Person collection cleaned.');
+    // Delete documents from Course collection
+    await Course.deleteMany({});
+    console.log('Course collection cleaned.');
 
   } catch (err) {
     console.error('Error cleaning collections:', err);
